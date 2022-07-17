e36f62b9e98a5c53bbea015e18461aca34b4dc82
cheeze2000 <me@cheeze2000.net>
2022-07-17 14:50:54 +0800

    feat: reader

---
+++ src/main/scala/iris/reader/Reader.scala
@@ -0,0 +1,23 @@
+package iris.reader
+
+type Position = (Int, Int)
+
+class Reader(input: String):
+	private var index = 0
+	private var line = 1
+	private var col = 0
+
+	def pos: Position = (line, col)
+
+	def hasPrefix(prefix: String): Boolean = input.startsWith(prefix, index)
+
+	def peek: Option[Char] = if index < input.length then Some(input.charAt(index)) else None
+
+	def next(): Unit =
+		if peek == Some('\n') then
+			line += 1
+			col = 0
+		else
+			col += 1
+
+		index += 1
---
+++ src/test/scala/iris/reader/ReaderSpec.scala
@@ -0,0 +1,57 @@
+package iris.reader
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers.shouldBe
+
+def assertReaderOutput(reader: Reader, output: List[(Char, Position)]): Unit =
+	for (c, pos) <- output do
+		reader.peek shouldBe Some(c)
+		reader.pos shouldBe pos
+		reader.next()
+
+class ReaderSpec extends AnyFlatSpec {
+	"Reader with empty input" should "output nothing" in {
+		val reader = Reader("")
+		assertReaderOutput(reader, List())
+	}
+
+	"Reader with single line input" should "output correct characters along with their positions" in {
+		val reader = Reader(
+			"""
+			ab c
+			""".stripIndent.trim
+		)
+
+		assertReaderOutput(reader, List(
+			('a', (1, 0)),
+			('b', (1, 1)),
+			(' ', (1, 2)),
+			('c', (1, 3))
+		))
+	}
+
+	"Reader with multiline input" should "output correct characters along with their positions" in {
+		val reader = Reader(
+			"""
+			a
+				b
+
+			 c
+			d
+			""".stripIndent.trim
+		)
+
+		assertReaderOutput(reader, List(
+			('a', (1, 0)),
+			('\n', (1, 1)),
+			('\t', (2, 0)),
+			('b', (2, 1)),
+			('\n', (2, 2)),
+			('\n', (3, 0)),
+			(' ', (4, 0)),
+			('c', (4, 1)),
+			('\n', (4, 2)),
+			('d', (5, 0))
+		))
+	}
+}
